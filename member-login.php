0404cee7834e53d1453e9c97bd95465a59aa7ecd
BeulaEvangelin <nicholbe@sheridancollege.ca>
2023-11-19 12:45:01 -0500

    created login and register page

---
+++ member-login.php
@@ -0,0 +1,10 @@
+<?php
+//member-login.php
+//login page for users who are already members
+?>
+
+<form action="process-member-login.php" method="POST">
+	<input name="username">
+	<input name="password">
+	<input type="submit">
+</form>
---
+++ user-register.php
@@ -0,0 +1,3 @@
+<?php
+//user_register.php
+//register page for public users who want to become members
