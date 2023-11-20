d1deea6ff6a6b09aff10ed5ff68b9240bb4f7cad
BeulaEvangelin <nicholbe@sheridancollege.ca>
2023-11-20 17:50:23 -0500

    created login page for members only

--- member-login.php
+++ member-login.php
@@ -3,8 +3,17 @@
 //login page for users who are already members
 ?>
 
-<form action="process-member-login.php" method="POST">
-	<input name="username">
-	<input name="password">
-	<input type="submit">
-</form>
+<section>
+<h1>Login</h1>
+<form name="userregister" action="process-member-login.php" method="POST">
+<fieldset>
+    <legend>Enter your IMMM MEMBER username and password</legend>
+
+        <label>Username:</label>
+        <input type="text" name="username" required>
+
+        <label>Password:</label>
+        <input type="text" name="password" required>
+
+        <input type="submit" value="Submit">
+</fieldset>
---
+++ process-member-login.php
@@ -0,0 +1,32 @@
+<?php session_start();
+//process-member-login.php
+//receive username and password POST variables
+$username = $_POST["username"];
+$password = $_POST["password"];
+$user_role = 'Member';
+
+//check the database for the username and password combination
+$dsn = "mysql:host=localhost;dbname=immnewsnetwork;charset=utf8mb4";
+$dbusername = "root";  
+$dbpassword = "";
+
+$pdo = new PDO($dsn, $dbusername, $dbpassword);
+$stmt = $pdo->prepare("SELECT `user_Id`, `username`, `role` 
+    FROM `user_role`
+	WHERE `username` = '$username' AND `password` = '$password' AND `role`= '$user_role' ;");
+$stmt->execute();
+
+//if u/p is correct, 
+if($row = $stmt->fetch()){
+	//show dashboard
+	?><p>Hello, <?=$username ?></p><?php
+	$_SESSION["user_Id"] = $row['user_Id'];
+	$_SESSION["username"] = $row['username'];
+	$_SESSION["loggedIn"] = true;
+	?><a href="member-articles.php">Back to IMM homepage</a><?php
+}else{
+	//else show error message
+	?><p>Error. <a href="member-login.php">Try login again</p><?php
+}
+
+?>
